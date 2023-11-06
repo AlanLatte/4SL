f84532d28b6654dabc0d0081c35227186c54a199
AlanLatte <shukurovfoteh@gmail.com>
2023-11-06 22:11:14 +0300

    [+] SSL checks by default [+] Docker support [+] Openssl check before running [~] Refactoring

--- ssl.sh
+++ ssl.sh
@@ -8,14 +8,14 @@ function generate_dhparam_ssl() {
         exit 1;
     fi
 
-    sudo openssl dhparam -out "$ssl_dhparam_path" 2048
-
     if [ -f "$ssl_dhparam_path" ]
       then
         echo "ssl_dhparam $ssl_dhparam_path;" >> "$nginx_snippets"
     else
         echo "Error: dhparam certs not Found, the certificate could not be added to nginx."
     fi
+
+    sudo openssl dhparam -out "$ssl_dhparam_path" 2048
 }
 
 function generate_ssl() {
@@ -36,102 +36,171 @@ function generate_ssl() {
     fi
 }
 
-function check_nginx(){
-  if ! nginx -v &> /dev/null
-    then
-      echo "Nginx  could not be found!"
-      exho "Use: sudo apt install nginx; for install them"
-      exit 1
+function check_dependencies(){
+
+  if ! openssl version &> /dev/null; then
+    echo "Openssl could not be found!"
+    echo "For install use: sudo apt install openssl -y"
+  fi
+
+  arr=("$@")
+  for i in "${arr[@]}";
+    do
+      case $i in
+        --nginx)
+          if ! nginx -v &> /dev/null; then
+            echo "Nginx  could not be found!"
+            echo "For install use: sudo apt install nginx -y"
+            exit 1
+          fi;;
+        --docker)
+          if ! docker -v &> /dev/null; then
+            echo "Docker could not be found!"
+            echo "For install use: curl -fsSL https://get.docker.com | sh"
+          fi;;
+        *)
+          echo "This dependencies is not supported now."
+          exit 1;;
+        esac
+    done
+}
+
+function cert_paths() {
+  files=()
+  dhparam=$1
+    if [ -z "$CERT_NAME" ]; then
+    echo "You haven't passed the \$domain. Read --help for see usage."
+    exit 1;
+  fi
+
+  if [ -n "$dhparam" ]; then
+    readonly ssl_dhparam_path="/etc/ssl/certs/$CERT_NAME.pem"
+    files+=("$ssl_dhparam_path")
+  fi
+
+  readonly ssl_certificate_path="/etc/ssl/certs/$CERT_NAME.crt"
+  files+=("$ssl_certificate_path")
+
+  readonly ssl_certificate_key_path="/etc/ssl/private/$CERT_NAME.key"
+  files+=("$ssl_certificate_key_path")
+
+  if [ "$CERT_TYPE" == "--nginx" ]; then
+    readonly nginx_snippets="/etc/nginx/snippets/ssl/$CERT_NAME"
+    files+=("$nginx_snippets")
   fi
 }
 
+function __linebreak() {
+  echo "----------------------------------"
+}
+
 function ssl() {
-    if [ -z "$DOMAIN_NAME" ]; then
-        echo "You haven't passed the domain. Read --help for see usage."
-        exit 1;
-    fi
+  read -r -p "Do you want to create self-signed ssl certs by $CERT_NAME? (y/n) " yn
 
-    readonly ssl_certificate_path="/etc/ssl/certs/$DOMAIN_NAME.crt"
-    readonly ssl_certificate_key_path="/etc/ssl/private/$DOMAIN_NAME.key"
-    readonly nginx_snippets="/etc/nginx/snippets/ssl/$DOMAIN_NAME"
-    readonly ssl_dhparam_path="/etc/ssl/certs/$DOMAIN_NAME.pem"
+  case $yn in
+    [yY] )
 
-    read -r -p "Do you want to create self-signed ssl certs by $DOMAIN_NAME? (y/n) " yn
+      if ! touch "$nginx_snippets"; then
+        mkdir -p "/etc/nginx/snippets/ssl/"
+        touch "$nginx_snippets"
+      fi
 
-    case $yn in
-      [yY] )
+      generate_ssl;
 
-        if ! touch "$nginx_snippets"; then
-          mkdir -p "/etc/nginx/snippets/ssl/"
-          touch "$nginx_snippets"
-        fi
-
-        generate_ssl;
+      if [ "$CERT_TYPE" == "--nginx" ]; then
         generate_dhparam_ssl;
-        ;;
-      [nN] ) echo exiting...;
-        exit;;
-      * ) echo invalid response;
-        exit 1;;
-    esac
+      fi
 
-    echo "Created ssl certificate on: $ssl_certificate_path"
-    echo "Created ssl certificate key on: $ssl_certificate_key_path"
+      ;;
+    [nN] ) echo exiting...;
+      exit;;
+    * ) echo invalid response;
+      exit 1;;
+  esac
+
+  __linebreak
+  echo "Created ssl certificate on: $ssl_certificate_path"
+  echo "Created ssl certificate key on: $ssl_certificate_key_path"
+
+  if [ "$CERT_TYPE" == "--nginx" ]; then
     echo "Created ssl dhparam on: $ssl_dhparam_path"
-    echo "----------------------------------"
+    __linebreak
     echo "Created Nginx snippets on: $nginx_snippets"
+  fi
 }
 
 function delete_ssl(){
-    readonly ssl_certificate_path="/etc/ssl/certs/$DOMAIN_NAME.crt"
-    readonly ssl_certificate_key_path="/etc/ssl/private/$DOMAIN_NAME.key"
-    readonly ssl_dhparam_path="/etc/ssl/certs/$DOMAIN_NAME.pem"
-    readonly nginx_snippets="/etc/nginx/snippets/ssl/$DOMAIN_NAME"
+  for index in "${!files[@]}" ; do
+      printf "%d. %s\n" "$index" "${files[$index]:-other}"
+  done
 
-    read -r -p "You really want to delete ssl certs by $DOMAIN_NAME? (y/n) " yn
+  read -r -p "You really want to delete this ssl certs? (y/n) " yn
 
-    case $yn in
-      [yY] )
-        readonly files=("$ssl_certificate_path" "$ssl_certificate_key_path" "$ssl_dhparam_path" "$nginx_snippets")
-        for file in "${files[@]}"; do
-          echo "Deleting: $file"
-          rm "$file"
-        done
-        ;;
-      [nN] ) echo exiting...;
-        exit;;
-      * ) echo invalid response;
-        exit 1;;
-    esac
+  case $yn in
+    [yY] )
+      for file in "${files[@]}"; do
+        echo "Deleting: $file"
+        rm "$file"
+      done
+      ;;
+    [nN] ) echo exiting...;
+      exit;;
+    * ) echo invalid response, exiting...;
+      exit 1;;
+  esac
+}
 
+function define_service_name() {
+  readonly CERT_TYPE=$1
+  if [ -z "$CERT_TYPE" ]; then
+    echo "You must choice between --docker and --nginx. Pass --help for see usage."
+    exit 1
+  fi
+  case $CERT_TYPE in
+    --docker)
+      if [ -n "$2" ]; then
+        echo "Error: Domain name not supported when you pass --docker argument."
+        exit 1
+      fi
+      readonly CERT_NAME="docker"
+      cert_paths
+      ;;
+    --nginx)
+      readonly CERT_NAME=$2
+      cert_paths 1
+      ;;
+    *)
+      echo "Error: Only --docker and --nginx service name supported"
+      exit 1;
+  esac
+
+  check_dependencies "$CERT_TYPE"
 }
 
 function process_arguments() {
-  check_nginx;
-
   if [[ $# -eq 0 ]]; then
     echo "Use -h/--help for see usage."
   fi
   while [[ $# -gt 0 ]]; do
     case $1 in
       -h|--help)
-        echo "-h/--help            - show usage.";
-        echo "-s/--ssl \$domain     - Create new ssl certificate.";
-        echo "-d/--delete \$domain  - Delete ssl certificate.";
+        echo "-h/--help                  - Show usage.";
+        echo "-n/--nginx \$domain         - Create new NGINX ssl certificate.";
+        echo "-nd/--nginx-delete \$domain - Delete ssl certificate.";
         echo "";
-        echo "Created by:       https://github.com/alan.latte";
+        echo "Created by:                 https://github.com/alan.latte";
         exit 1;;
       -s|--ssl)
-        readonly DOMAIN_NAME="$2"
         shift
-        ssl "$DOMAIN_NAME";
-        break ;;
-      -d|--delete)
-        readonly DOMAIN_NAME="$2"
-        delete_ssl "$DOMAIN_NAME";
-        shift
+        define_service_name "$1" "$2"
+        ssl;
         break;;
-      -*|--*)
+      -d|--delete)
+        shift
+        define_service_name "$1" "$2"
+        delete_ssl;
+        break;;
+      *)
         echo "Command not found. Use -h/--help for see usage."
         exit 1;;
     esac
@@ -139,3 +208,6 @@ function process_arguments() {
 }
 
 process_arguments "$@";
+__linebreak
+echo "Thanks for usage. Created by alan.latte ☕ "
+
